6ac32c78c4381bc284c76de4bd8949eb1e527645
buchtawill <buchtawill@gmail.com>
2024-08-04 22:52:42 -0400

    Change clock freq to 16MHz

--- Rev2_F401RE/cubeide_proj/Core/Src/main.c
+++ Rev2_F401RE/cubeide_proj/Core/Src/main.c
@@ -168,7 +168,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLM = 8;
   RCC_OscInitStruct.PLL.PLLN = 168;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 7;
